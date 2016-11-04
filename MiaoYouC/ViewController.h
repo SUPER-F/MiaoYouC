3a223e63e8b4aec7cb1aa8643d138009a2e57346
SUPER-F <drupem@drupemdeimac-2.local>
2016-11-04 10:09:34 +0800

    Initial Commit

---
+++ MiaoYouC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiaoYouC
+//
+//  Created by drupem on 16/11/4.
+//  Copyright © 2016年 drupem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MiaoYouCTests/MiaoYouCTests.m
@@ -0,0 +1,39 @@
+//
+//  MiaoYouCTests.m
+//  MiaoYouCTests
+//
+//  Created by drupem on 16/11/4.
+//  Copyright © 2016年 drupem. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MiaoYouCTests : XCTestCase
+
+@end
+
+@implementation MiaoYouCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
